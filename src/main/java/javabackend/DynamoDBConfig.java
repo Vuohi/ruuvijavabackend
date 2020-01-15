faad2076e5820c04bce77fb7b27a94bd644cdea6
Katri Vuori <ktrvuori@gmail.com>
2020-01-15 18:47:21 +0200

    a small reminder to hit ctrl s

--- src/main/java/javabackend/DynamoDBConfig.java
+++ src/main/java/javabackend/DynamoDBConfig.java
@@ -14,7 +14,6 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -27,10 +26,10 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class DynamoDBConfig {
     
-    @Value("${spring.profiles.active.contains(\"production\")?System.getenv(\"S3_KEY\"):${amazon.dynamodb.accesskey}}")
-    private String amazonDynamoDBAccessKey;// = System.getenv("S3_KEY");
-    @Value("${spring.profiles.active.contains(\"production\")?System.getenv(\"S3_SECRET\"):${amazon.dynamodb.secretkey}}")
-    private String amazonDynamoDBSecretKey;// = System.getenv("S3_SECRET");
+    //@Value("${spring.profiles.active.contains(\"production\")?System.getenv(\"S3_KEY\"):${amazon.dynamodb.accesskey}}")
+    private String amazonDynamoDBAccessKey = System.getenv("S3_KEY");
+    //@Value("${spring.profiles.active.contains(\"production\")?System.getenv(\"S3_SECRET\"):${amazon.dynamodb.secretkey}}")
+    private String amazonDynamoDBSecretKey = System.getenv("S3_SECRET");
     
 
     public AWSCredentialsProvider amazonAWSCredentialsProvider() {
